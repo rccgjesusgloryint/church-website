d4db252be33a4d2dfb0abee4ce7f7508cac5792c
Joshf225 <joshuafamose@gmail.com>
2024-09-17 17:47:11 +0100

    cleaned up code in services file

--- src/app/(main)/about/Services.tsx
+++ src/app/(main)/about/Services.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-import Image from "next/image";
-
-import { CiLogin } from "react-icons/ci";
-import { GoArrowUpRight } from "react-icons/go";
-import Link from "next/link";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
-
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useSession } from "next-auth/react";
+
+import ServiceCards from "./ServiceCards";
 
 const Services = () => {
   const subTitle2 = React.useRef<HTMLElement | any>();
@@ -33,8 +23,6 @@ const Services = () => {
       opacity: 0,
     });
   });
-
-  const { status } = useSession();
 
   return (
     <section className="h-auto w-full relative">
@@ -57,142 +45,7 @@ const Services = () => {
             </h1>
           </div>
         </div>
-        <div className="flex flex-row justify-between items-center flex-wrap gap-5 mt-[100px] w-full mb-11 ">
-          <div className="sm:w-[331px] w-full h-[460px] bg-slate-400 flex flex-col justify-end items-start pb-14 pl-10 gap-1 bg-bible-studies bg-center bg-cover">
-            <HoverCard>
-              <HoverCardTrigger className="cursor-pointer">
-                <Image
-                  src="/images/Zoom.png"
-                  alt="location"
-                  width={40}
-                  height={40}
-                />
-              </HoverCardTrigger>
-              <HoverCardContent>
-                {status === "authenticated" ? (
-                  <div>
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_ZOOM_LINK}`}
-                      className="flex gap-2 items-center justify-center"
-                      target="_blank"
-                    >
-                      Join Meeting{" "}
-                      <span>
-                        <GoArrowUpRight />
-                      </span>
-                    </Link>
-                    <p>Zoom Password :sdfsdfdsf</p>
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center justify-center gap-4">
-                    <p className="text-xl">Sensitive data! Please log in</p>
-                    <Link href="/api/auth/signin">
-                      <CiLogin size={35} />
-                    </Link>
-                  </div>
-                )}
-              </HoverCardContent>
-            </HoverCard>
-            <div className="backdrop-blur-xl">
-              <h3 className="font-bold text-lg">Spiritual Growth</h3>
-              <h1 className="font-bold text-3xl">Bible Studies</h1>
-            </div>
-          </div>
-          <div className="sm:w-[331px] w-full h-[460px] bg-slate-400 flex flex-col justify-end items-start pb-14 pl-10 gap-1 bg-sunday-service bg-center bg-cover">
-            <HoverCard>
-              <HoverCardTrigger className="cursor-pointer">
-                <Image
-                  src="/images/Zoom.png"
-                  alt="location"
-                  width={40}
-                  height={40}
-                />
-              </HoverCardTrigger>
-              <HoverCardContent>
-                {status === "authenticated" ? (
-                  <div>
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_ZOOM_LINK}`}
-                      className="flex gap-2 items-center justify-center"
-                      target="_blank"
-                    >
-                      Join Meeting{" "}
-                      <span>
-                        <GoArrowUpRight />
-                      </span>
-                    </Link>
-                    <p>Zoom Password :sdfsdfdsf</p>
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center justify-center gap-4">
-                    <p className="text-xl">Sensitive data! Please log in</p>
-                    <Link href="/api/auth/signin">
-                      <CiLogin size={35} />
-                    </Link>
-                  </div>
-                )}
-              </HoverCardContent>
-            </HoverCard>
-            <div className="backdrop-blur-xl">
-              <h3 className="font-bold text-lg">Fellowship</h3>
-              <h1 className="font-bold text-3xl">Sunday Service</h1>
-            </div>
-          </div>
-          <div className="sm:w-[331px] w-full h-[460px] bg-slate-400 flex flex-col justify-end items-start pb-14 pl-10 gap-1 bg-prayers bg-center bg-cover">
-            <HoverCard>
-              <HoverCardTrigger className="cursor-pointer">
-                <Image
-                  src="/images/Zoom.png"
-                  alt="location"
-                  width={40}
-                  height={40}
-                />
-              </HoverCardTrigger>
-              <HoverCardContent>
-                {status === "authenticated" ? (
-                  <div>
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_ZOOM_LINK}`}
-                      className="flex gap-2 items-center justify-center"
-                      target="_blank"
-                    >
-                      Join Meeting{" "}
-                      <span>
-                        <GoArrowUpRight />
-                      </span>
-                    </Link>
-                    <p>Zoom Password :sdfsdfdsf</p>
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center justify-center gap-4">
-                    <p className="text-xl">Sensitive data! Please log in</p>
-                    <Link href="/api/auth/signin">
-                      <CiLogin size={35} />
-                    </Link>
-                  </div>
-                )}
-              </HoverCardContent>
-            </HoverCard>
-            <div className="backdrop-blur-xl">
-              <h3 className="font-bold text-lg">Spiritual Growth</h3>
-              <h1 className="font-bold text-3xl">Friday Prayers</h1>
-            </div>
-          </div>
-          <div className="sm:w-[331px] w-full h-[460px] bg-slate-400 flex flex-col justify-end items-start pb-14 pl-10 gap-1 bg-evangelism bg-center bg-cover">
-            <span>
-              {/* <Image
-            src="/images/Zoom.png"
-            alt="location"
-            width={40}
-            height={40}
-          /> */}
-            </span>
-            <div className="backdrop-blur-2xl">
-              <h3 className="font-bold text-lg">Soul Winning</h3>
-              <h1 className="font-bold text-3xl">Evangelism</h1>
-            </div>
-          </div>
-        </div>
+        <ServiceCards />
       </div>
     </section>
   );
