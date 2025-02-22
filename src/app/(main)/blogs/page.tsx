f63a89f159a53fdb75439cda9b4768989357dd51
Joshf225 <joshuafamose@gmail.com>
2025-02-22 17:22:40 +0000

    added loading component to blogs page

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -8,9 +8,11 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { getAllBlogs } from "@/lib/queries";
 import { Blog } from "@prisma/client";
+import Loader from "../../../../components/Loader";
 
 const Blogs = () => {
   const [blogs, setBlogs] = React.useState<Blog[]>();
+  const [isLoading, setIsLoading] = React.useState(false);
   const router = useRouter();
 
   const handleBlogRedirection = (blogId: string) => {
@@ -19,99 +21,109 @@ const Blogs = () => {
 
   React.useEffect(() => {
     const getData = async () => {
+      setIsLoading(true);
       const response = await getAllBlogs();
       setBlogs(response);
+      setIsLoading(false);
     };
     getData();
   }, []);
 
-  if (!blogs) return <div className="h-screen">No Blogs </div>;
+  // if (!blogs) return false;
 
   return (
     <>
       <Navbar2 />
       <div className="h-screen w-full">
-        <div className="flex flex-col flex-wrap gap-3 items-center pt-[10rem]">
-          {blogs.length > 0 && (
-            <div
-              className="flex flex-col gap-5 max-h-[800px] relative"
-              key={blogs[0]?.id}
-            >
-              <div className="absolute top-[25rem] right-[-15rem]">
-                <h1 className="font-medium text-[20px] mb-3">All categories</h1>
-                <div className="flex flex-col">
-                  {TEMP_BLOG_CATEGOORIES.map((category) => (
-                    <span className="cursor-pointer" key={category}>
-                      {category}
-                    </span>
-                  ))}
-                </div>
-              </div>
-              <div className="flex flex-col gap-3">
-                <div className="">
-                  {blogs[0]?.createdAt.toDateString().slice(3)}
-                </div>
-                <h1
-                  className="font-bold text-4xl cursor-pointer hover:text-gray-600"
-                  onClick={() => handleBlogRedirection(blogs[0]?.id)}
-                >
-                  {blogs[0]?.blogTitle}
-                </h1>
-              </div>
-              <Image
-                alt="poster-image"
-                src={
-                  blogs[0]?.blogImage ||
-                  "https://www.1689designs.com/cdn/shop/files/all-over-print-flag-white-front-6604d51e7e80c.png?v=1711592746"
-                }
-                width={500}
-                height={500}
-                className="cursor-pointer hover:opacity-80 max-h-[500px] object-contain"
-                onClick={() => handleBlogRedirection(blogs[0]?.id)}
-              />
-              <h1 className="font-normal">{blogs[0]?.blogDescription}</h1>
-            </div>
-          )}
-          {blogs?.length !== 0 ? (
-            blogs
-              ?.filter((num, index) => index > 0)
-              .map((blog, index) => (
-                <div
-                  className="flex flex-row items-start gap-5 py-10 max-w-[800px] border-t-[1px] border-[#e0e0e0]"
-                  key={blog.id}
-                >
-                  <Image
-                    alt="poster-image"
-                    src={
-                      blog.blogImage ||
-                      "https://www.1689designs.com/cdn/shop/files/all-over-print-flag-white-front-6604d51e7e80c.png?v=1711592746"
-                    }
-                    width={200}
-                    height={150}
-                    className="cursor-pointer hover:opacity-80"
-                    onClick={() => handleBlogRedirection(blog.id)}
-                  />
-                  <div className="flex flex-col gap-3">
-                    <div className="">
-                      {blog.createdAt.toDateString().slice(3)}
-                    </div>
-                    <h1
-                      className="font-bold text-2xl cursor-pointer hover:text-gray-600"
-                      onClick={() => handleBlogRedirection(blog.id)}
-                    >
-                      {blog.blogTitle}
-                    </h1>
-                    <h1 className="font-normal">
-                      {/* TO DO// When creating the form for posting blogs make the description cap at 450 characters */}
-                      {blog.blogDescription.slice(0, 450)}
-                    </h1>
+        {isLoading ? (
+          <div className="h-screen flex items-center justify-center">
+            <Loader />
+          </div>
+        ) : (
+          <div className="flex flex-col flex-wrap gap-3 items-center pt-[10rem]">
+            {blogs && blogs.length > 0 && (
+              <div
+                className="flex flex-col gap-5 max-h-[800px] relative"
+                key={blogs[0]?.id}
+              >
+                <div className="absolute top-[25rem] right-[-15rem]">
+                  <h1 className="font-medium text-[20px] mb-3">
+                    All categories
+                  </h1>
+                  <div className="flex flex-col">
+                    {TEMP_BLOG_CATEGOORIES.map((category) => (
+                      <span className="cursor-pointer" key={category}>
+                        {category}
+                      </span>
+                    ))}
                   </div>
                 </div>
-              ))
-          ) : (
-            <div className="h-screen">No blogs have been posted yet</div>
-          )}
-        </div>
+                <div className="flex flex-col gap-3">
+                  <div className="">
+                    {blogs[0]?.createdAt.toDateString().slice(3)}
+                  </div>
+                  <h1
+                    className="font-bold text-4xl cursor-pointer hover:text-gray-600"
+                    onClick={() => handleBlogRedirection(blogs[0]?.id)}
+                  >
+                    {blogs[0]?.blogTitle}
+                  </h1>
+                </div>
+                <Image
+                  alt="poster-image"
+                  src={
+                    blogs[0]?.blogImage ||
+                    "https://www.1689designs.com/cdn/shop/files/all-over-print-flag-white-front-6604d51e7e80c.png?v=1711592746"
+                  }
+                  width={500}
+                  height={500}
+                  className="cursor-pointer hover:opacity-80 max-h-[500px] object-contain"
+                  onClick={() => handleBlogRedirection(blogs[0]?.id)}
+                />
+                <h1 className="font-normal">{blogs[0]?.blogDescription}</h1>
+              </div>
+            )}
+            {blogs?.length !== 0 ? (
+              blogs
+                ?.filter((num, index) => index > 0)
+                .map((blog, index) => (
+                  <div
+                    className="flex flex-row items-start gap-5 py-10 max-w-[800px] border-t-[1px] border-[#e0e0e0]"
+                    key={blog.id}
+                  >
+                    <Image
+                      alt="poster-image"
+                      src={
+                        blog.blogImage ||
+                        "https://www.1689designs.com/cdn/shop/files/all-over-print-flag-white-front-6604d51e7e80c.png?v=1711592746"
+                      }
+                      width={200}
+                      height={150}
+                      className="cursor-pointer hover:opacity-80"
+                      onClick={() => handleBlogRedirection(blog.id)}
+                    />
+                    <div className="flex flex-col gap-3">
+                      <div className="">
+                        {blog.createdAt.toDateString().slice(3)}
+                      </div>
+                      <h1
+                        className="font-bold text-2xl cursor-pointer hover:text-gray-600"
+                        onClick={() => handleBlogRedirection(blog.id)}
+                      >
+                        {blog.blogTitle}
+                      </h1>
+                      <h1 className="font-normal">
+                        {/* TO DO// When creating the form for posting blogs make the description cap at 450 characters */}
+                        {blog.blogDescription.slice(0, 450)}
+                      </h1>
+                    </div>
+                  </div>
+                ))
+            ) : (
+              <div className="h-screen">No blogs have been posted yet</div>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
