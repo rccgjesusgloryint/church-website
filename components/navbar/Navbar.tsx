c917b9fde768bb92a9bc52cdae74333c1366b060
Joshf225 <joshuafamose@gmail.com>
2025-06-04 21:59:23 +0100

    fixed navbar bug

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -10,24 +12,29 @@ import Link from "next/link";
 import { AuthButton, MobileViewNavbar, navContent } from ".";
 import { accessCheck, isAdmin } from "@/lib/queries";
 import { Role } from "@prisma/client";
+import Loader from "../Loader";
 
 const Navbar = () => {
   const navbar = React.useRef<HTMLElement | any>();
   const [admin, setAdmin] = React.useState<boolean | null>(null);
   const [userRole, setUserRole] = React.useState<Role | undefined>();
+  const [loaded, setIsLoaded] = React.useState(false);
 
   React.useEffect(() => {
-    const checkUserAdmin = async () => {
+    setIsLoaded(false);
+    const navbarCheck = async () => {
       const res = await isAdmin();
       setAdmin(res);
-    };
-    const checkUserRole = async () => {
       const userRole = await accessCheck();
       setUserRole(userRole);
+      setIsLoaded(true);
     };
-    checkUserAdmin();
-    checkUserRole();
+    navbarCheck();
   }, []);
+
+  React.useEffect(() => {
+    console.log(loaded);
+  }, [loaded]);
 
   useGSAP(() => {
     gsap.from(navbar.current, {
@@ -38,31 +45,40 @@ const Navbar = () => {
   });
 
   return (
-    <div className="w-screen">
-      <div className="relative">
-        <MobileViewNavbar />
-        <div className="hidden sm:flex flex-row font-normal gap-7 justify-center items-center cursor-pointer h-full pt-11 mb-[10rem]">
-          {navContent.map(({ label, link }) => (
-            <Link
-              href={link}
-              key={label}
-              className={`${
-                admin === null ||
-                (admin === false && label === "Admin") ||
-                admin === null ||
-                (userRole === undefined && label === "Blogs")
-                  ? "hidden"
-                  : ""
-              } hover:text-gray-700 duration-200`}
-            >
-              {label}
-            </Link>
-          ))}
-          <AuthButton />
+    <>
+      {loaded ? (
+        // setTimeout(()
+        <div className="w-screen">
+          <div className="relative">
+            <MobileViewNavbar />
+            <div className="hidden sm:flex flex-row font-normal gap-7 justify-center items-center cursor-pointer h-full pt-11 mb-[10rem]">
+              {navContent.map(({ label, link }) => (
+                <Link
+                  href={link}
+                  key={label}
+                  className={`${
+                    admin === null ||
+                    (admin === false && label === "Admin") ||
+                    admin === null ||
+                    (userRole === undefined && label === "Blogs")
+                      ? "hidden"
+                      : ""
+                  } hover:text-gray-700 duration-200`}
+                >
+                  {label}
+                </Link>
+              ))}
+              <AuthButton />
+            </div>
+            <AuthButton />
+          </div>
         </div>
-        <AuthButton />
-      </div>
-    </div>
+      ) : (
+        <div className="w-screen flex items-center justify-center relative">
+          <Skeleton className="h-[15px] w-[30px] px-10 my-10 opacity-5" />
+        </div>
+      )}
+    </>
   );
 };
 
--- components/navbar/Navbar2.tsx
+++ components/navbar/Navbar2.tsx
@@ -8,23 +8,25 @@ import React from "react";
 import { AuthButton2, MobileViewNavbar2, navContent } from ".";
 import { accessCheck, isAdmin } from "@/lib/queries";
 import { Role } from "@prisma/client";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Navbar2 = () => {
   const [admin, setAdmin] = React.useState<boolean | null>(null);
   const [userRole, setUserRole] = React.useState<Role | undefined>();
+  const [loaded, setIsLoaded] = React.useState(false);
 
   React.useEffect(() => {
-    const checkUserAdmin = async () => {
+    setIsLoaded(false);
+    const navbarCheck = async () => {
       const res = await isAdmin();
       setAdmin(res);
-    };
-    const checkUserRole = async () => {
       const userRole = await accessCheck();
       setUserRole(userRole);
+      setIsLoaded(true);
     };
-    checkUserAdmin();
-    checkUserRole();
+    navbarCheck();
   }, []);
+
   return (
     <div className="bg-white h-[100px] shadow-md w-full flex items-center justify-center relative">
       <Link
@@ -41,29 +43,35 @@ const Navbar2 = () => {
       <div className="hidden md:flex absolute top-5 right-5">
         <AuthButton2 />
       </div>
-      <div className="h-full sm:flex sm:justify-end">
-        <div className="sm:hidden h-full w-full flex items-center justify-center">
-          <MobileViewNavbar2 />
+      {loaded ? (
+        <div className="h-full sm:flex sm:justify-end">
+          <div className="sm:hidden h-full w-full flex items-center justify-center">
+            <MobileViewNavbar2 />
+          </div>
+          <div className="hidden md:flex flex-row gap-9 justify-center items-center w-full 2xl:flex-wrap relative mr-10">
+            {navContent.map(({ label, link }) => (
+              <Link
+                href={link}
+                key={label}
+                className={`${
+                  admin === null ||
+                  (admin === false && label === "Admin") ||
+                  admin === null ||
+                  (userRole === undefined && label === "Blogs")
+                    ? "hidden"
+                    : ""
+                } hover:text-gray-700 duration-200`}
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
         </div>
-        <div className="hidden md:flex flex-row gap-9 justify-center items-center w-full 2xl:flex-wrap relative mr-10">
-          {navContent.map(({ label, link }) => (
-            <Link
-              href={link}
-              key={label}
-              className={`${
-                admin === null ||
-                (admin === false && label === "Admin") ||
-                admin === null ||
-                (userRole === undefined && label === "Blogs")
-                  ? "hidden"
-                  : ""
-              } hover:text-gray-700 duration-200`}
-            >
-              {label}
-            </Link>
-          ))}
+      ) : (
+        <div className="w-screen flex items-center justify-center relative">
+          <Skeleton className="h-[15px] w-[30px] px-10 my-10 opacity-5" />
         </div>
-      </div>
+      )}
     </div>
   );
 };
