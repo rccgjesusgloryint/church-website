3051a1eacb45c95e483dc658e2b375fdffce3f1c
Joshf225 <joshuafamose@gmail.com>
2025-11-05 22:27:27 +0000

    refactored: restyled event page

--- components/media/file-upload.tsx
+++ components/media/file-upload.tsx
@@ -10,27 +10,6 @@ type Props = {
 };
 
 const FileUpload = ({ onChange, value }: Props) => {
-  const [url, setUrl] = useState<string>("");
-  const [previewUrl, setPreviewUrl] = useState<string>("");
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!url) {
-      return alert("NO LINKS");
-    }
-    const form = new FormData(e.currentTarget);
-    try {
-      const response = await fetch(`/api/upload/file`, {
-        method: "POST",
-        body: form,
-      });
-
-      setUrl(response.url as string);
-      console.log("url: ", url);
-    } catch (error) {
-      alert(`Whoops ${error}`);
-    }
-  };
   return (
     <div className="space-y-2">
       <Label htmlFor="post-image">
--- src/app/(main)/(events)/events/[id]/index.tsx
+++ src/app/(main)/(events)/events/[id]/index.tsx
@@ -1,79 +1,15 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { EventDescription } from "@/lib/types";
-
-import { useModal } from "@/providers/modal-provider";
-
-import Image from "next/image";
-import CustomModal from "../../../../../../components/global/custom-modal";
-import { FALLBACK } from "@/app/(main)/gallery/GalleryModal";
-
-interface EventProps {
-  event: EventDescription;
+interface EventCardProps {
+  image: string;
 }
 
-export const BreadCrumb = () => {
+export function ImageCard({ image }: EventCardProps) {
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/events">Events</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Event</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
-  );
-};
-
-export const Event = ({ event }: EventProps) => {
-  const { setOpen } = useModal();
-  return (
-    <div>
-      {event?.description.eventPosterImage ? (
-        <div>
-          <Image
-            src={event.description.eventPosterImage || FALLBACK}
-            alt="poster-image"
-            width={500}
-            height={500}
-            onClick={() =>
-              setOpen(
-                <CustomModal title="" subheading="">
-                  <Image
-                    src={event.description.eventPosterImage || FALLBACK}
-                    alt="poster-image"
-                    width={1000}
-                    height={1500}
-                  />
-                </CustomModal>
-              )
-            }
-            className={"hidden sm:flex"}
-          />
-          <Image
-            src={event.description.eventPosterImage || FALLBACK}
-            alt="poster-image"
-            width={300}
-            height={300}
-            className={"flex sm:hidden"}
-          />
-        </div>
-      ) : (
-        <h1>Loading...</h1>
-      )}
+    <div className="flex items-center justify-center">
+      <img
+        src={image || "/placeholder.svg"}
+        alt={"title"}
+        className="w-1/2 h-1/2 object-contain transition-transform duration-300 hover:scale-105"
+      />
     </div>
   );
-};
+}
